3dfbcaa631067201809689cafeb0137c5b49b866
MilinPagare <pagare.milin1996@gmail.com>
2023-12-18 22:42:05 +0530

    progressbar using single div completed

--- src/components/UsingSingleDiv.js
+++ src/components/UsingSingleDiv.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import "./UsingSingleDiv.css";
+
 const UsingSingleDiv = () => {
-  return <div>Hello</div>;
+  const [percentageCompletion, setPercentageCompletion] = useState(0);
+
+  const start = () => {
+    for (let i = 1; i <= 100; i++) {
+      setTimeout(() => {
+        setPercentageCompletion(i);
+      }, 1000 * i);
+    }
+  };
+
+  return (
+    <>
+      <h1>Using Single Div</h1>
+      <div className="progressbar">
+        <div
+          style={{ width: `${percentageCompletion}%` }}
+          className="inner_progressbar"
+        ></div>
+      </div>
+      <button onClick={start}>Start</button>
+    </>
+  );
 };
 
 export default UsingSingleDiv;
